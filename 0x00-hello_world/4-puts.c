d1e3bcf92ca05a5ac1c9414f0225a7de305be322
Dalyop <isiquedan@gmail.com>
2023-03-17 01:39:37 -0700

    Corrected 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints the text Programming is like building a multilingual puzzle
+ * returns (0) - Always success
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
