2c27173c94ea0308939ff8385b7e451cdf1ce60f
Dalyop <isiquedan@gmail.com>
2023-03-20 05:50:28 -0700

    Corrected betty style error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,9 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/**main - generates a random number
+ * if else - tells if the number is positive, negative or just a zero value
+ */
 int main(void)
 {
 	int n;
@@ -10,13 +13,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-	}else if (n==0)
+	}else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	}else if (n<0)
+	}else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
