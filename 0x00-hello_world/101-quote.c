bfde9775b9ec5da5e7038c301a2313910c3f8ff9
Dalyop <isiquedan@gmail.com>
2023-03-20 18:08:52 -0700

    Corrected 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - prints the string "and that piece of ate is useful"
  * -Dora Korpar, 2015-10-19
  *  Followed by a new line
- *  Returns 1
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
